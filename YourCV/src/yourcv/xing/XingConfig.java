affddfea5abc3d703c790acecab7e184de048ea9
Victor Wuendisch <victor.wuendisch@sap.com>
2012-10-12 16:40:15 +0200

    newest commit

--- YourCV/src/yourcv/xing/XingConfig.java
+++ YourCV/src/yourcv/xing/XingConfig.java
@@ -5,5 +5,5 @@ public class XingConfig {
 	public static String _CONSUMERSECRET = "aa6caca071b06faba54ed53e911bd222b33d78c0";
 	public static String _SIGNATUREMETHOD = "PLAINTEXT";
 	public static String _BASEURL ="https://api.xing.com/v1/";
-	public static String _CALLBACKURL ="http://localhost:8888/XingAuth";
+	public static String _CALLBACKURL ="http://grabyourcv.appspot.com/XingAuth";
 }
